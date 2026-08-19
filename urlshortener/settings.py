452acdd0aac59904656fa808db695e905d5acc85
suhanpoojary666 <suhanpoojary666@gmail.com>
2026-08-19 16:31:49 +0530

    update ALLOWED_HOSTS for production

--- urlshortener/settings.py
+++ urlshortener/settings.py
@@ -25,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rckq4a5_9z_kzgx^0kpt3#gtmujup)%hv$+o_)5xr67m#6uz_^'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["url-shortener-wj7g.onrender.com",]
 
 
 # Application definition
@@ -60,7 +60,6 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5500",
-    "http://localhost:5500",
 ]
 
 ROOT_URLCONF = 'urlshortener.urls'
