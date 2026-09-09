7fadee2a1d5d47fc22ff2c04e90477af6b403b09
suhanpoojary666 <suhanpoojary666@gmail.com>
2026-09-09 17:50:06 +0530

    resolve the background worker issue

--- shortener/apps.py
+++ shortener/apps.py
@@ -1,6 +1,25 @@
+import os
+import threading
+
 from django.apps import AppConfig
 
 
 class ShortenerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'shortener'
+
+    def ready(self):
+        if os.environ.get("RUN_MAIN") == "true":
+            return
+        if os.environ.get("CELERY_WORKER_STARTED"):
+            return
+        os.environ["CELERY_WORKER_STARTED"] = "true"
+
+        from urlshortener.celery_client import app as celery_app
+
+        def start_worker():
+            celery_app.worker_main(
+                argv=["worker", "--loglevel=info", "--pool=solo"]
+            )
+
+        threading.Thread(target=start_worker, daemon=True).start()
